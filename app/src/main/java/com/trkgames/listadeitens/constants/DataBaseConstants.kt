6969c3230a8508e96dd6f6d86e82ae3516bfda83
Helvio Siqueira <67134795+HelvioSiqueira@users.noreply.github.com>
2023-02-25 16:46:45 -0300

    feat: add room

---
+++ app/src/main/java/com/trkgames/listadeitens/constants/DataBaseConstants.kt
@@ -0,0 +1,10 @@
+package com.trkgames.listadeitens.constants
+
+const val DATABASE_NAME = "dbListItems"
+const val DATABASE_VERSION = 1
+const val TABLE_LIST = "listItems"
+const val COLUMN_ID = "_id"
+const val COLUMN_NAME_ITEM = "nameItem"
+const val COLUMN_DESCRIPTION = "description"
+const val COLUMN_PRICE = "price"
+const val COLUMN_CATEGORY = "categories"
